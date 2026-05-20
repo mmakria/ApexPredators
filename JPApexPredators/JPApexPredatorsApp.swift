d8a4a44f42d50d8b6fc2c5fcf74311ca50c14da1
momomak <m.makria@gmail.com>
2026-05-20 17:53:22 +0200

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Mohamed Makria on 20/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
