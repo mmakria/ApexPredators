f0cc834cfe5e902d021139dd874f3a4da1db5c35
momomak <m.makria@gmail.com>
2026-05-20 20:59:41 +0200

    feat: first commit + add predator models and JSON decoding

---
+++ JPApexPredators/ApexPredator.swift
@@ -0,0 +1,53 @@
+//
+//  Predators.swift
+//  JPApexPredators
+//
+//  Created by Mohamed Makria on 20/05/2026.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct ApexPredator: Decodable , Identifiable{
+    let id: Int
+    let name: String
+    let type: APType
+    let latitude: Double
+    let longitude: Double
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+    
+    var image : String {
+        self.name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
+    struct MovieScene: Decodable, Identifiable{
+        let id: Int
+        let movie: String
+        let sceneDescription: String
+    }
+    
+    
+    enum APType: String, Decodable {
+        
+        case land
+        case air
+        case sea
+        
+        var background : Color {
+            switch self {
+            case .land:
+                    .brown
+            case .air:
+                    .teal
+            case .sea:
+                    .blue
+            }
+        }
+    }
+}
+
+
+
