4aeb0f945edc65847663c153ada74eb5d07e619b
momomak <m.makria@gmail.com>
2026-05-22 20:32:47 +0200

    feat: filter and sort function implemented

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -9,33 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     let predators = Predators()
+    @State var searchText = ""
+    @State var alphabetical = false
+    
+    var filteredDinos: [ApexPredator]{
+        predators.sort(by: alphabetical)
+        return predators.search(for: searchText)
+    }
     
     var body: some View {
-        
-        List(predators.apexPredators){ predator in
-            HStack{
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack(alignment: .leading){
-                    //name
+        NavigationStack{
+            List(filteredDinos){ predator in
+                NavigationLink{
                     Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    //type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
-                       
+                }label: {
+                   HStack {
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        
+                        VStack(alignment: .leading){
+                            //name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            //type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle("Apex predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
+            .toolbar{
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        alphabetical.toggle()
+                        
+                    } label: {
+                        if alphabetical {
+                            Image(systemName: "film")
+                        } else {
+                            Image(systemName: "textformat")
+                        }
+                        
+                    }
                 }
             }
+            
         }
         .preferredColorScheme(.dark)
     }
